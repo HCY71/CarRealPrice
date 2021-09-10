6d82d0f0ee39bf5ef756bd1e1631640f982db27b
Win Chiu <win881206@gmail.com>
2021-09-11 01:22:12 +0800

    backend user register

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+const app = express();
+
+connectDB();
+app.use(express.json());
+
+app.use('/api/users', require('./routes/api/user'));
+
+app.get('*', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
